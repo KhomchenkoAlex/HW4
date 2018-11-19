e96ced053de2eec36ee974c97a9edce3c804e501
Khomchenko_Alex <khomchenkoalex79@gmail.com>
2018-11-19 10:55:56 +0200

    add days name to day_fragment

---
+++ app/src/main/java/com/example/android/viewpager/enums/Days.kt
@@ -0,0 +1,11 @@
+package com.example.android.viewpager.enums
+
+enum class Days(name: String) {
+    MONDAY("Monday"),
+    TUESDAY("Tuesday"),
+    WEDNESDAY("Wednesday"),
+    THURSDAY("Thursday"),
+    FRIDAY("Friday"),
+    SATURDAY("Saturday"),
+    SUNDAY("Sunday")
+}
