a0061809481a2c66a137159183b0f11ea577f853
Khomchenko_Alex <khomchenkoalex79@gmail.com>
2018-11-20 23:14:42 +0200

    refactoring code

---
+++ app/src/main/java/com/example/android/viewpager/adapters/RecyclerViewAdapter.kt
@@ -0,0 +1,35 @@
+package com.example.android.viewpager.adapters
+
+import android.support.v7.widget.RecyclerView
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import com.example.android.viewpager.R
+
+class RecyclerViewAdapter(
+        private val items: List<String>)
+    : RecyclerView.Adapter<RecyclerViewAdapter.ViewHolder>() {
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        val inflater = LayoutInflater.from(parent.context)
+        val itemView = inflater.inflate(R.layout.text_item, parent, false)
+        return ViewHolder(itemView)
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        holder.onBind(items[position])
+    }
+
+    override fun getItemCount(): Int {
+       return items.size
+    }
+
+    inner class ViewHolder(itemView: View): RecyclerView.ViewHolder(itemView){
+        private val itemText: TextView = itemView.findViewById(R.id.text_view)
+
+        fun onBind(item: String) {
+            itemText.text = item
+        }
+    }
+}
